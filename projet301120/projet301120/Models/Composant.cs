d6d2a9b63965f2ce6d60706b483f4d13128ffab0
Galaad LUSVEN <60004615+Galaad-LSV@users.noreply.github.com>
2020-11-30 09:14:27 +0100

    Ajout classze

---
+++ projet301120/projet301120/Models/Composant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet301120.Models
+{
+    class Composant
+    {
+        #region Attributs
+        #endregion
+        #region Constructeurs
+        #endregion
+        #region Getters-Setteurs
+        #endregion
+        #region Methodes
+        #endregion
+    }
+}
