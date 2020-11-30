d98f090b13fd782618d1b3cbd757f7870ad2ce19
Galaad LUSVEN <60004615+Galaad-LSV@users.noreply.github.com>
2020-11-30 09:25:22 +0100

    Composant fini

--- projet301120/projet301120/Models/Composant.cs
+++ projet301120/projet301120/Models/Composant.cs
@@ -9,10 +9,26 @@ namespace projet301120.Models
     public class Composant
     {
         #region Attributs
+        public static List<Composant> CollClasseComposant = new List<Composant>();
+        private int _id;
+        private string _nomIngredients;
+        private int _quantite;
+        private double _prix;
         #endregion
         #region Constructeurs
+        public Composant(int id, string nomIngredients, int quantite, double prix)
+        {
+            _id = id;
+            _nomIngredients = nomIngredients;
+            _quantite = quantite;
+            _prix = prix;
+        }
         #endregion
         #region Getters-Setteurs
+        public int Id { get => _id; set => _id = value; }
+        public string NomIngredients { get => _nomIngredients; set => _nomIngredients = value; }
+        public int Quantite { get => _quantite; set => _quantite = value; }
+        public double Prix { get => _prix; set => _prix = value; }
         #endregion
         #region Methodes
         #endregion
