bab6d922956c66c68a8fa2660337e0ad4d7dfd8f
Galaad LUSVEN <60004615+Galaad-LSV@users.noreply.github.com>
2020-11-30 09:45:24 +0100

    Classse Galaad

--- projet301120/projet301120/Models/Emporter.cs
+++ projet301120/projet301120/Models/Emporter.cs
@@ -14,10 +14,11 @@ namespace projet301120.Models
         #region Constructeurs
         public Emporter(int id, string leNom, double lePrix, int reduction): base(id, leNom, lePrix)
         {
-
+            _reduction = reduction;
         }
         #endregion
         #region Getters-Setteurs
+        public int Reduction { get => _reduction; set => _reduction = value; }
         #endregion
         #region Methodes
         #endregion
--- projet301120/projet301120/Models/SurPlace.cs
+++ projet301120/projet301120/Models/SurPlace.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace projet301120.Models
 {
-    public class SurPlace
+    public class SurPlace: Pizza
     {
         #region Attributs
-
+        private string _cadeau;
         #endregion
         #region Constructeurs
+        public SurPlace(string cadeau, int id, string leNom, double lePrix) : base(id, leNom, lePrix)
+        {
+            _cadeau = cadeau;
+        }
         #endregion
         #region Getters-Setteurs
+        public string Cadeau { get => _cadeau; set => _cadeau = value; }
         #endregion
         #region Methodes
         #endregion
